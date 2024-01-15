996e5a6d1213263a9f12d34fa863f7a5b10a75d4
cslabiano <cslabiano@up.edu.ph>
2024-01-15 21:27:13 +0800

    chore: add page title

--- frontend/src/pages/admin/view.js
+++ frontend/src/pages/admin/view.js
@@ -1,10 +1,16 @@
 import Navbar from "../../components/AdminNavbar.js";
 import React from "react";
+import styles from "./View.module.css";
 
 const View = () => {
   return (
     <>
-      <Navbar />
+      <div className={styles.container}>
+        <Navbar />
+        <div className={styles.title}>
+          <h4 className={styles.h4}>File Requests</h4>
+        </div>
+      </div>
     </>
   );
 };
