7a7f503e5fa795cda4f50623ecd59926befc7e8c
Yan1an <cgberana@up.edu.ph>
2024-01-23 02:15:03 +0800

    feat: add user dashboard and folderView

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import Login from "./pages/authorization/login.js";
 import Register from "./pages/authorization/register.js";
 import RequestForm from "./pages/user/requestForm.js";
 import View from "./pages/admin/view.js";
+import UserDashboard from "./pages/user/dashboard.js";
+import AdminDashboard from "./pages/admin/dashboard.js";
+import UserFolderView from "./pages/user/folderView.js";
+import AdminFolderView from "./pages/user/folderView.js";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/admin/view" element={<View />} />
         <Route path="/user/form" element={<RequestForm/>} />
+        <Route path="/user/dashboard" element={<UserDashboard/>} />
+        <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+        <Route path="/user/folderView" element={<UserFolderView/>} />
+        <Route path="/admin/folderView" element={<AdminFolderView/>} />
       </Routes>
     </>
   );
---
+++ frontend/src/pages/user/folderView.js
@@ -0,0 +1,42 @@
+import Navbar from "../../components/userNavbar.js";
+import Folders from "../../components/folders.js";
+import Files from "../../components/files.js";
+import React from "react";
+import { FaAngleRight } from "react-icons/fa";
+import styles from "./folderView.module.css";
+
+const UserFolderView = () => {
+    return (
+        <>
+            <div><Navbar /></div>
+            <div className={styles.partition}>
+                <p className={styles.heading}> 
+                    Root 
+                    <FaAngleRight fontSize={30} />
+                    CMSC 
+                </p>
+
+                <p className={styles.subHeading}>
+                    Folders
+                </p>
+                <div className={styles.container}>
+                    <Folders title={"CMSC 12"}/>
+                    <Folders title={"CMSC 22"}/>
+                    <Folders title={"CMSC 123"}/>
+                    <Folders title={"CMSC 130"}/>
+                    <Folders title={"CMSC 150"}/>
+                </div>
+
+                <p className={styles.subHeading}>
+                    Files
+                </p>
+                <div className={styles.container}>
+                    <Files title={"BSCS Curriculum"} type={"pdf"}/>
+                    <Files title={"BSCS Curriculum"} type={"docs"}/>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default UserFolderView;
