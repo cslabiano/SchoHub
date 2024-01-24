e8ab0500fca448f31dd78d6c7bc016a6d8a12756
mcrecaplaza <mcrecaplaza@up.edu.ph>
2024-01-24 22:18:04 +0800

    feat: fixed request form page

--- frontend/src/pages/user/requestForm.js
+++ frontend/src/pages/user/requestForm.js
@@ -47,13 +47,17 @@ const RequestForm = () => {
                 ></input>
               </div>
               <div class="col-10">
-                <label for="inputAddress2" class="form-label">
+                <p className={styles.fileFormat1}>Enter the file name in this format:</p>
+                <p className={styles.fileFormat2}>&lt;Subject&gt;_&lt;SubjectCode&gt;_&lt;Module#/Exam#&gt;_&lt;TypeOfMaterial&gt;</p>
+              </div>
+              <div class="col-10">
+                <label for="purpose" class="form-label">
                   Purpose of Request
                 </label>
                 <input
                   type="text"
                   class="form-control"
-                  id="inputAddress2"
+                  id="purposeReq"
                   placeholder="State why you requested the material"
                 ></input>
               </div>
@@ -61,7 +65,24 @@ const RequestForm = () => {
           </div>
         </div>
 
-        <div className={styles.right}></div>
+        <div className={styles.right}>
+          <p className={styles.rightTitle}>Guidelines in requesting a material</p>
+          <div className={styles.textBox}>
+            <p className={styles.textGuide}>
+              The Scholastics Department holds the authority to both accept and reject material requests based on a thorough assessment of their alignment with our curriculum, educational standards, and the core values of the organization.
+            </p>
+            <p className={styles.textGuide}>
+              Every material in the SchoHub, including the materials to be requested, shall be under the discretion of the organization and not to be shared among those outside of it.
+            </p>
+            <p className={styles.textGuide}>
+              Any misuse of materials will be subject to investigation, and potential consequences, including disciplinary action, will be determined based on the severity of the offense.
+            </p>
+          </div>
+          <div className={styles.buttonBox}>
+            <button type="submit" class="btn btn-light">Submit</button>
+          </div>
+          
+        </div>
       </div>
     </>
   );
